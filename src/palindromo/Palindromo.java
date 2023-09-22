00f9d34b8ecb062dd50d0d3256bb39722ad28e32
luism <luism@LuxhPC>
2023-09-21 18:48:39 -0700

    Se agregó comentarios.

--- src/palindromo/Palindromo.java
+++ src/palindromo/Palindromo.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author luism
+ * @author Luis Blasco
  */
 public class Palindromo {
 
@@ -16,16 +16,17 @@ public class Palindromo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Scanner para poner nuestro palindromo.
         System.out.println("Ingrese el texto aqui: ");
         Scanner entrada = new Scanner(System.in);
         String oracion = entrada.nextLine();
         
+        // La siguiente variable empieza a poner todos los caracteres en minuscula y reemplaza todos los caracteres que no nos interesan.
         String oracionCompleta = oracion.toLowerCase().replace("!","").replace("¡","").replace(",","").replace(" ", "").replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u");
+        // La siguiente variable invierte los caracteres de nuestro String oracionCompleta
         String oracionInvertida = new StringBuilder(oracionCompleta).reverse().toString();
         
-        System.out.println(oracionCompleta);
-        System.out.println(oracionInvertida);
-        
+        // Preguntamos que si la oracion (bien estructurada) es igual a la oracion inversa.
         if(oracionCompleta.equals(oracionInvertida)){
             System.out.println("Si es palindromo");
         } else{
